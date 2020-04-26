2f93320cfa3c631e131941d107e3ccf6c441ad3b
AnamariaGoldan <48368498+AnamariaGoldan@users.noreply.github.com>
2020-04-26 17:19:31 +0300

    Add files via upload

---
+++ js/new_filtre.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.gototop').click(function(event){
+      event.preventDefault()
+      $('html,body').animate({scrollTop:0},1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $('.button-load-more-images').click(function(event){
+      event.preventDefault()
+      $('html,body').animate({scrollTop:0},1000);
+    });
+  });
