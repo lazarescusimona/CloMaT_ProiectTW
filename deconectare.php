dc756b3d540e505794294106361c8d9260b55b90
AnamariaGoldan <48368498+AnamariaGoldan@users.noreply.github.com>
2020-06-11 06:38:29 +0300

    Delete deconectare.php

--- deconectare.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-  unset($_SESSION['username']);
-  unset($_SESSION['email']);
-  unset($_SESSION['sex']);
-  unset($_SESSION['areRuda']);
-  header("Location: index.php");
